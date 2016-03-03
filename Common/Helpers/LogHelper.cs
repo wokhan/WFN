8aede61a37416aeafb8a31d2dd9d4301ba4c92ab
DanielPharos_cp <SND\DanielPharos_cp>
2016-03-03 09:49:11 +0000

    Don't show code locations in releases! That's an information disclosure security vulnerability.

--- Common/Helpers/LogHelper.cs
+++ Common/Helpers/LogHelper.cs
@@ -54,49 +54,85 @@ namespace Wokhan.WindowsFirewallNotifier.Common.Helpers
             }
         }
 
+#if DEBUG
         public static void Debug(string msg,
             [CallerMemberName] string memberName = null,
             [CallerFilePath] string filePath = null,
             [CallerLineNumber] int lineNumber = -1)
+#else
+        public static void Debug(string msg)
+#endif
         {
             if (Settings.Default.EnableVerboseLogging)
             {
+#if DEBUG
                 writeLog("DEBUG", msg, memberName, filePath, lineNumber);
+#else
+                writeLog("DEBUG", msg);
+#endif
             }
         }
 
+#if DEBUG
         public static void Info(string msg,
             [CallerMemberName] string memberName = null,
             [CallerFilePath] string filePath = null,
             [CallerLineNumber] int lineNumber = -1)
+#else
+        public static void Info(string msg)
+#endif
         {
             if (Settings.Default.EnableVerboseLogging)
             {
+#if DEBUG
                 writeLog("INFO", msg, memberName, filePath, lineNumber);
+#else
+                writeLog("INFO", msg);
+#endif
             }
         }
 
+#if DEBUG
         public static void Warning(string msg,
             [CallerMemberName] string memberName = null,
             [CallerFilePath] string filePath = null,
             [CallerLineNumber] int lineNumber = -1)
+#else
+        public static void Warning(string msg)
+#endif
         {
+#if DEBUG
             writeLog("WARNING", msg, memberName, filePath, lineNumber);
+#else
+            writeLog("WARNING", msg);
+#endif
         }
 
+#if DEBUG
         public static void Error(string msg, Exception e,
             [CallerMemberName] string memberName = null,
             [CallerFilePath] string filePath = null,
             [CallerLineNumber] int lineNumber = -1)
+#else
+        public static void Error(string msg, Exception e)
+#endif
         {
-            writeLog("ERROR", msg + Environment.NewLine + (e != null ? e.Message + Environment.NewLine + e.StackTrace : ""), memberName, filePath, lineNumber);
+            writeLog("ERROR", msg + Environment.NewLine + (e != null ? e.Message + Environment.NewLine + e.StackTrace : "")
+#if DEBUG
+, memberName, filePath, lineNumber
+#endif
+                );
         }
 
         private static readonly EventWaitHandle locker = new EventWaitHandle(true, EventResetMode.AutoReset, "WFN_Log_Sync_Lock");
+#if DEBUG
         private static void writeLog(string type, string msg,
             string memberName = null,
             string filePath = null,
             int lineNumber = -1)
+#else
+        private static void writeLog(string type, string msg)
+#endif
         {
             System.Diagnostics.Debug.WriteLine(msg);
 
@@ -105,6 +141,7 @@ namespace Wokhan.WindowsFirewallNotifier.Common.Helpers
             {
                 using (var sw = new StreamWriter(logFilePath, true))
                 {
+#if DEBUG
                     var codeLocation = string.Empty;
                     if (!string.IsNullOrWhiteSpace(memberName)
                         || !string.IsNullOrWhiteSpace(filePath))
@@ -122,6 +159,14 @@ namespace Wokhan.WindowsFirewallNotifier.Common.Helpers
                         type,
                         msg,
                         codeLocation);
+#else
+                    sw.WriteLine("{0:yyyy/MM/dd HH:mm:ss} - {1} [{2}] - [{3}] {4}",
+                        DateTime.Now,
+                        Environment.UserName,
+                        isAdmin,
+                        type,
+                        msg);
+#endif
                 }
             }
             finally
