33264052b8a001b04d0f3904368feb862ff14838
DanielPharos_cp <AtlasHackert@users.noreply.github.com>
2016-03-03 10:28:01 +0000

    Removed unneeded includes.

--- Common/Helpers/LogHelper.cs
+++ Common/Helpers/LogHelper.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Threading;
-using System.Windows;
 
 namespace Wokhan.WindowsFirewallNotifier.Common.Helpers
 {
