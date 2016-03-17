f5b6cc7c2864048655293134c129e92938dc6578
DanielPharos_cp <SND\DanielPharos_cp>
2016-03-17 10:43:36 +0000

    Forgot to add some log output.

--- Common/Helpers/IPHelpers/TCP6Helper.cs
+++ Common/Helpers/IPHelpers/TCP6Helper.cs
@@ -142,11 +142,13 @@ namespace Wokhan.WindowsFirewallNotifier.Common.Helpers.IPHelpers
                 if ((retn != NO_ERROR) && (retn != ERROR_INSUFFICIENT_BUFFER))
                 {
                     //Cannot get owning module for this connection
+                    LogHelper.Info("Unable to get the connection owner.");
                     return ret;
                 }
                 if (buffSize == 0)
                 {
                     //No buffer? Probably means we can't retrieve any information about this connection; skip it
+                    LogHelper.Info("Unable to get the connection owner.");
                     return ret;
                 }
                 buffer = Marshal.AllocHGlobal((int)buffSize);
