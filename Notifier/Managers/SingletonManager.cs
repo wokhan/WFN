431152640bd4bff8babaac25ae8667b015b72dc9
DanielPharos_cp <AtlasHackert@users.noreply.github.com>
2016-10-29 15:47:18 +0000

    Fixed a race condition with multiple Notifiers starting at the same time.

--- Notifier/Managers/SingletonManager.cs
+++ Notifier/Managers/SingletonManager.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.VisualBasic.ApplicationServices;
 using System;
+using System.Threading;
 
 namespace Wokhan.WindowsFirewallNotifier.Notifier.Managers
 {
     class SingletonManager : WindowsFormsApplicationBase, IDisposable
     {
         private App application;
+
+        private Mutex mutex = new Mutex();
 
         public SingletonManager()
         {
@@ -15,8 +18,16 @@ namespace Wokhan.WindowsFirewallNotifier.Notifier.Managers
 
         protected override bool OnStartup(Microsoft.VisualBasic.ApplicationServices.StartupEventArgs e)
         {
-            application = new App(e.CommandLine);
-            application.Run();
+            mutex.WaitOne();
+            try
+            {
+                application = new App(e.CommandLine);
+                application.Run();
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
 
             return false;
         }
@@ -28,19 +39,31 @@ namespace Wokhan.WindowsFirewallNotifier.Notifier.Managers
             //Give focus to the main instance
             e.BringToForeground = true;
 
+            //There is a race condition where the original program might already have shutdown. Let's try and work around that.
+            //Note: Using double-checked locking here.
             if (application == null)
             {
-                //There is a race condition where the original program might already have shutdown. Let's try and work around that.
-                application = new App(e.CommandLine);
-                application.Run();
-                return;
+                mutex.WaitOne();
+                try
+                {
+                    if (application == null)
+                    {
+                        application = new App(e.CommandLine);
+                        application.Run();
+                        return;
+                    }
+                }
+                finally
+                {
+                    mutex.ReleaseMutex();
+                }
             }
             application.NextInstance(e.CommandLine);
         }
 
         public void Dispose()
         {
-            
+            mutex.Dispose();
         }
     }
 }
