60852fcafe39d2d4ddeea089a55d143575b9a3de
DanielPharos_cp <SND\DanielPharos_cp>
2016-12-03 22:43:10 +0000

    Cleaned up some initialization code, and added (currently non-function) function to detect fullscreen applications so that the Notifier doesn't steal focus.

---
+++ Common/Helpers/WindowHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Wokhan.WindowsFirewallNotifier.Common.Helpers
+{
+    public class WindowHelper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static bool isSomeoneFullscreen()
+        {
+            //Determine if there is a fullscreen window currently active (with focus).
+            //FIXME: Implement!
+            return false;
+        }
+    }
+}
