7bb5c3f28f461431f66db593903e0add040bd25f
harrwiss <33778373+harrwiss@users.noreply.github.com>
2019-10-21 21:25:53 +0200

    Revert "Fixed potential XML parsing vulnerabilities."
    This reverts commit 7c38c47827d586b2a75c7eaa4443114a365abf06.

--- Common/Helpers/NetshHelper.cs
+++ Common/Helpers/NetshHelper.cs
@@ -29,9 +29,10 @@ namespace Wokhan.WindowsFirewallNotifier.Common.Helpers
                 RunResult rr = runCommandCapturing(sys32Folder + @"\netsh.exe", @"wfp show filters file=-");
                 if (rr.exitCode == 0)
                 {
-                    xmlDoc = new XmlDocument() { XmlResolver = null };
-                    XmlReader reader = XmlReader.Create(rr.outputData.ToString(), new XmlReaderSettings() { XmlResolver = null });
-                    xmlDoc.Load(reader);
+                    xmlDoc = new XmlDocument
+                    {
+                        InnerXml = rr.outputData.ToString()
+                    };
                 }
                 else
                 {
@@ -52,7 +53,7 @@ namespace Wokhan.WindowsFirewallNotifier.Common.Helpers
         {
             XmlNode root = doc.DocumentElement;
 
-            // Add the namespace.
+            // Add the namespace.  
             XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
 
             try
