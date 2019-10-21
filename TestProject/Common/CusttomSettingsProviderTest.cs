4a3f7a377cca267fd2be3725c4f39683d7cc9b2f
harrwiss <33778373+harrwiss@users.noreply.github.com>
2019-10-21 20:11:24 +0200

    Update test project

---
+++ TestProject/Common/CusttomSettingsProviderTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wokhan.WindowsFirewallNotifier.Common;
+using Wokhan.WindowsFirewallNotifier.Common.Helpers;
+
+namespace TestProject.Common
+{
+    [TestClass]
+    public class CustomSettingsProviderTest
+    {
+        [TestMethod]
+        ///
+        /// Tests whether global settings are properly initialized from this test project (path names to config files e.g. WFN.config)
+        /// If not, check the Build > Output path
+        /// 
+        public void testInit()
+        {
+            Assert.IsNotNull(Settings.Default);
+
+            // is the path to the global config file WFN.config correct?
+            Assert.IsTrue(File.Exists(CustomSettingsProvider.SharedConfigurationPath));
+
+            // can we read the first run parameter?
+            Assert.IsTrue(Settings.Default.FirstRun);
+        }
+    }
+}
