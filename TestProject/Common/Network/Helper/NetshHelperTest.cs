1bc8a4eb201089e597270bde594f8ef905135632
harrwiss <33778373+harrwiss@users.noreply.github.com>
2019-10-27 13:00:00 +0100

    NetshHelper tests

--- TestProject/Common/Network/Helper/NetshHelperTest.cs
+++ TestProject/Common/Network/Helper/NetshHelperTest.cs
@@ -16,16 +16,19 @@ namespace TestProject.Common.Network.Helper
         [TestMethod]
         public void TestGetMatchingFilterInfo()
         {
-            FilterResult result = NetshHelper.getMatchingFilterInfo(1208377); // "Block Inbound Default Rule"
+            //FilterResult result = NetshHelper.getMatchingFilterInfo(1260694); 
+            FilterResult result = NetshHelper.getMatchingFilterInfo(104337); // Default Outbound (at the end of the xml)
             Assert.IsNotNull(result);
             Console.WriteLine($"name={result.Name}, description={result.Description}");
-            Assert.AreEqual("Block Inbound Default Rule", result.Name);
-            Assert.IsFalse(string.IsNullOrEmpty(result.Description));
+            //Assert.AreEqual("Boot Time Filter", result.Name, true);
+            Assert.IsTrue(result.Description != null);
         }
+
+        [TestMethod]
         public void TestGetMatchingFilterInfo_notfound()
         {
             Assert.IsTrue(UacHelper.CheckProcessElevated(), "Only admin can run this test - restart as admin");
-            FilterResult result = NetshHelper.getMatchingFilterInfo(65785); // allows localhost in/out connections but does not exist in filters 
+            FilterResult result = NetshHelper.getMatchingFilterInfo(65785); // allows localhost in/out connections but does not exist in netsh wfp filters 
             Assert.IsNull(result);
         }
     }
