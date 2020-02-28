626a801e81aa769340e3edf23909ff1738d8d841
harrwiss <33778373+harrwiss@users.noreply.github.com>
2020-02-28 13:52:13 +0100

    Add enum extensions class

---
+++ Common/Extensions/EnumExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wokhan.WindowsFirewallNotifier.Common.Extensions
+{
+    public static class EnumExtensions
+    {
+        /*
+         * Get an enum description.
+         * 
+         * Usage:
+         *  public enum MyEnum
+            {
+                [Description("Description for Foo")]
+                Foo,
+                [Description("Description for Bar")]
+                Bar
+            }
+         * 
+         */
+        public static string GetDescription(this Enum value)
+        {
+            Type type = value.GetType();
+            string name = Enum.GetName(type, value);
+            if (name != null)
+            {
+                FieldInfo field = type.GetField(name);
+                if (field != null)
+                {
+                    DescriptionAttribute attr =
+                           Attribute.GetCustomAttribute(field,
+                             typeof(DescriptionAttribute)) as DescriptionAttribute;
+                    if (attr != null)
+                    {
+                        return attr.Description;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
