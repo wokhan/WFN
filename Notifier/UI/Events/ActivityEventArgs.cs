5ac6615dde7e6c96a16d6619994980a6de22eb46
harrwiss <33778373+harrwiss@users.noreply.github.com>
2020-03-01 14:18:37 +0100

    Add StaticActivityPublisher (experimental)

---
+++ Notifier/UI/Events/ActivityEventArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wokhan.WindowsFirewallNotifier.Notifier.Helpers;
+
+namespace Wokhan.WindowsFirewallNotifier.Notifier.UI.Events
+{
+    public class ActivityEventArgs : EventArgs
+    {
+        public ActivityEnum Activity { get; set; }
+
+        public CurrentConn CurrentConnection { get; set; }
+
+        public DateTime TimeStamp { get; } = DateTime.Now;
+
+        public enum ActivityEnum
+        {
+            Allow, Block, AllowTemp, BlockTemp
+        }
+        public ActivityEventArgs(ActivityEnum activity, CurrentConn currentConnection)
+        {
+            Activity = activity;
+            CurrentConnection = currentConnection;
+        }
+    }
+}
