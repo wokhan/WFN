34e180ca8ed7be0ff760a59c86f196ead5bd574a
harrwiss <33778373+harrwiss@users.noreply.github.com>
2020-03-08 17:08:03 +0100

    Migrate console.tests  to get them running again

---
+++ Common.Tests/Helpers/LogHelperTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Wokhan.WindowsFirewallNotifier.Common.Helpers
+{
+    // Manual tests for loghelper functionality
+    [TestClass]
+    public class LogHelperTest
+    {
+        [TestMethod]
+
+        public void TestInit()
+        {
+            Assert.IsNotNull(Settings.Default);
+
+            // is the path to the global config file WFN.config correct?
+            Assert.IsTrue(File.Exists("WFN.config.log4net"));
+
+            LogHelper.Info("info log message");
+
+            LogHelper.Debug("debug log message");
+
+        }
+    }
+}
