a163ff44f30b61b4887eb909b9dad6b50b6c2f81
Khan <wokhan@outlook.com>
2020-05-09 10:55:00 +0200

    First batch of UI updates. Updated to Wokhan.UI instead of Wokhan.WPF.

---
+++ Console/UI/Controls/ToolBarToggleButton.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace Wokhan.WindowsFirewallNotifier.Console.UI.Controls
+{
+    public class ToolBarToggleButton : ToggleButton
+    {
+        public bool IsToggle { get; } = true;
+
+        [Bindable(true)]
+        public string Text { get; set; }
+        public static DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(ToolBarToggleButton), new PropertyMetadata(null));
+
+        [Bindable(true)]
+        public object Image { get; set; }
+        public static DependencyProperty ImageProperty = DependencyProperty.Register("Image", typeof(object), typeof(ToolBarToggleButton), new PropertyMetadata(null));
+
+        [Bindable(true)]
+        public Brush ImageBrush { get; set; }
+        public static DependencyProperty ImageBrushProperty = DependencyProperty.Register(nameof(ImageBrush), typeof(Brush), typeof(ToolBarToggleButton), new PropertyMetadata(null));
+
+        [Bindable(true)]
+        public Orientation Orientation { get; set; }
+
+        static ToolBarToggleButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ToolBarToggleButton), new FrameworkPropertyMetadata(typeof(ToolBarToggleButton)));
+        }
+    }
+}
